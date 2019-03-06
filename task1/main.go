85d442573966b68501b3a58f32b8ade87d1eb9df
artyom <artyom.bukhavtsov@gmail.com>
2019-03-07 01:19:37 +0300

    add task_1

--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,20 @@
 package main
 
-// Specify Filter function here
+import "fmt"
+
+func Filter(numbers []int, predicate func(int, int) bool) (arr []int) {
+	for _, element := range numbers {
+		if predicate(element, element+1) {
+			arr = append(arr, element)
+		}
+	}
+	return arr
+}
+func isEvenNumber(a int, b int) bool {
+	return a%2 == 0
+}
 
 func main() {
-
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println(Filter(numbers, isEvenNumber))
 }

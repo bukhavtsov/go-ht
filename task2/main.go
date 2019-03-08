eb18a94b7ef09d6d320d6aa58a047cb411003494
artyom <artyom.bukhavtsov@gmail.com>
2019-03-08 18:37:52 +0300

    add task_2

--- task2/main.go
+++ task2/main.go
@@ -1,10 +1,48 @@
 package main
 
-// Specify MapTo function here
+import (
+	"fmt"
+	"strconv"
+)
 
-func Convert(arr []int) []string {
-	// converting nimber code should be here
+func MapTo(numbers []int, makeWord func(elem int, _ int) string) (stringElements []string) {
+	for index, number := range numbers {
+		stringElements = append(stringElements, makeWord(number, index))
+	}
+	return
+}
+
+func makeWord(element, _ int) string {
+	elementStr := strconv.Itoa(element)
+	switch elementStr {
+	case "1":
+		return "one"
+	case "2":
+		return "two"
+	case "3":
+		return "three"
+	case "4":
+		return "four"
+	case "5":
+		return "five"
+	case "6":
+		return "six"
+	case "7":
+		return "seven"
+	case "8":
+		return "eight"
+	case "9":
+		return "nine"
+	default:
+		return "unknown"
+	}
+}
+
+func Convert(numbers []int) []string {
+	return MapTo(numbers, makeWord)
 }
 
 func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("%T", MapTo(numbers, makeWord))
 }

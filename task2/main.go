7564b017f1d9cf8916c25b042af063d54d0abe19
artyom <artyom.bukhavtsov@gmail.com>
2019-03-13 20:46:51 +0300

    task-2 has been done

--- task2/main.go
+++ task2/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func MapTo(numbers []int, makeWord func(elem int, _ int) string) (stringElements []string) {
@@ -13,24 +12,23 @@ func MapTo(numbers []int, makeWord func(elem int, _ int) string) (stringElements
 }
 
 func makeWord(element, _ int) string {
-	elementStr := strconv.Itoa(element)
-	value, ok := wordsMap[elementStr]
+	value, ok := wordsMap[element]
 	if ok {
 		return value
 	}
 	return "unknown"
 }
 
-var wordsMap = map[string]string{
-	"1": "one",
-	"2": "two",
-	"3": "three",
-	"4": "four",
-	"5": "five",
-	"6": "six",
-	"7": "seven",
-	"8": "eight",
-	"9": "nine",
+var wordsMap = map[int]string{
+	1: "one",
+	2: "two",
+	3: "three",
+	4: "four",
+	5: "five",
+	6: "six",
+	7: "seven",
+	8: "eight",
+	9: "nine",
 }
 
 func Convert(numbers []int) []string {
